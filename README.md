4f8429f3540122204478bd78fe59f83774677297
Rene Merino <rmerino@amayamedia.com>
2015-07-31 18:54:28 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Install Dependencies
+
+```bash
+$ npm install
+$ bower install
+```
+
+This project uses Skeleton-sass (css grid system) which needs a quick setup to get it to work. On prompt enter `gtmsquared`.
+
+```bash
+$ cd bower_components/skeleton-sass && bin/setup.rb
+```
+Now open `_gtmsquared.config.scss` and add variables below to the end of the file
+
+```bash
+$is-fluid: true;
+$base-col-count: 12;
+```
+
+You can now run `gulp watch` for development or just `gulp` for production.
+
+Happy coding!
---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var spritesmith = require('gulp.spritesmith');
+var merge = require('merge-stream');
+
+var path = {
+	src: {
+		scss: 'scss',
+		js: 'js',
+		vendor: 'js/vendor',
+		img: 'img',
+		bower: 'bower_components'
+	},
+	dest: {
+		css: 'css',
+		js: 'js',
+		img: 'img'
+	}
+}
+
+/**
+ * SCSS for development
+ */
+gulp.task('sass:dev', function () {
+	return gulp.src(path.src.scss + '/**/*.scss')
+		.pipe(
+			sass().on('error', sass.logError)
+		)
+		.pipe(
+			autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest(path.dest.css));
+});
+
+/**
+ * SCSS for production
+ */
+gulp.task('sass', function () {
+	return gulp.src(path.src.scss + '/**/*.scss')
+		.pipe(
+			sass({
+				outputStyle: 'compressed'
+			})
+			.on('error', sass.logError)
+		)
+		.pipe(
+			autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest(path.dest.css));
+});
+
+/**
+ * Compile plugins
+ */
+gulp.task('compile_scripts', function() {
+	return gulp.src([
+			path.src.vendor + '/jquery-1.11.3.min.js',
+			path.src.vendor + '/modernizr-2.8.3.min.js',
+			path.src.bower + '/underscore/underscore.js',
+			path.src.bower + '/jquery-backstretch/jquery.backstretch.js'
+		])
+		.pipe(concat('plugins.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(path.dest.js))
+});
+
+/**
+ * Dev js
+ */
+gulp.task('main_js:dev', function() {
+	return gulp.src(path.src.js + '/main.js')
+		.pipe(concat('main.min.js'))
+		.pipe(gulp.dest(path.dest.js))
+});
+
+/**
+ * Compile main js file
+ */
+gulp.task('main_js', function() {
+	return gulp.src(path.src.js + '/main.js')
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(path.dest.js))
+});
+
+/**
+ * Watch task
+ */
+gulp.task('watch', function () {
+	gulp.watch(path.src.scss + '/**/*.scss', ['sass:dev']);
+	gulp.watch(path.src.js + '/**/*.js', ['main_js:dev']);
+});
+
+/**
+ * Sprite task
+ */
+gulp.task('sprite', function () {
+	var spriteData = gulp.src(path.src.img + '/sprites/*.png')
+		.pipe(
+			spritesmith({
+				padding: 5,
+				imgPath: '../img/sprites.png',
+				imgName: 'sprites.png',
+				cssName: '_sprites.scss'
+			})
+		);
+
+		// Pipe image stream through image optimizer and onto disk
+  		var imgStream = spriteData.img
+			.pipe(gulp.dest(path.dest.img));
+
+		// Pipe CSS stream through CSS optimizer and onto disk
+  		var cssStream = spriteData.css
+			.pipe(gulp.dest(path.src.scss + '/components'))
+
+		// Return a merged stream to handle both `end` events
+		return merge(imgStream, cssStream);
+});
+
+/**
+ * Cache breaker task
+ */
+
+/**
+ * Default task
+ */
+gulp.task('default', function() {
+	runSequence('sprite', 'sass', 'compile_scripts', 'main_js')
+});
